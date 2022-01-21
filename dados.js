c10fa19108b87703274e4e2276ec4b2d1627cdbf
bielvbcg <gabrielvbcallegario@gmail.com>
2022-01-21 19:42:37 -0300

    requisitos implementados

---
+++ dados.js
@@ -0,0 +1,15 @@
+const usuarios = [
+  {
+    username: 'bobesponja',
+    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+  },
+]
+
+const tweets = [
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  }
+]
+
+export const dados = { usuarios, tweets }
--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 import express from "express"
 import cors from "cors"
+//import { usuarios, tweets } from "dados.js"
 
 const server = express()
 server.use(cors())
+server.use(express.json())
+
+const usuarios = []
+const tweets = []
+
+server.post("/sign-up", (req, res) => {
+  usuarios.push(req.body)
+  res.send("OK")
+})
+
+server.post("/tweets", (req, res) => {
+  tweets.push(req.body)
+  res.send("OK")
+})
+
+server.get("/tweets", (req, res) => {
+  const ultimosTweets = []
+
+  tweets.map((tweet, index) => {
+    const _tweet = {
+      username: tweet.username,
+      avatar: usuarios.find(usuario => usuario.username === tweet.username).avatar,
+      tweet: tweet.tweet,
+    }
+
+    index < 10 ? ultimosTweets.push(_tweet) : ""
+  })
+
+  res.send(ultimosTweets)
+})
 
 server.listen(5000)
